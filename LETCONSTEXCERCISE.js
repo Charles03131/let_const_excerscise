dea3ede7712823f7af297b6c3d1495be91b8138a
Charles03131 <97250801+Charles03131@users.noreply.github.com>
2022-06-27 21:21:09 -0500

    Add files via upload

---
+++ LETCONSTEXCERCISE.js
@@ -0,0 +1,30 @@
+
+
+
+
+const PI=3.14;
+
+
+//QUIZ
+
+
+/* WHAT IS THE DIFFERENCE BETWEEN VAR AND LET?
+
+var is globally scopped or function scoped 
+it can be updated or changed. let is block scoped, it can  be updated
+(but not redeclared).
+
+WHAT IS THE DIFFERENCE BETWEEN VAR AND CONST?
+var is globally scoped or function scoped, it can be updated or chnaged,
+ Const is block scoped and cannot be changed. 
+
+WHAT IS THE DIFFERENCE BETWEEN LET AND CONST?
+
+let and const are both block scoped but let can be changed and const cannot.
+let can be declared without initializing but const has to be declared at initialization.
+
+
+WHAT IS HOISTING?
+
+hoisting is where variables and functon declarations are moved to the top of their scope/block 
+before the code executes.
